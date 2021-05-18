46deac486e3eb72eb46c73816b3a7de553066b27
Devashish Pise <devashishpise6@gmail.com>
2021-05-18 00:01:33 -0700

    day2 assignments

---
+++ src/day2/ArithmaticOperator.java
@@ -0,0 +1,29 @@
+package day2;
+
+public class ArithmaticOperator {
+
+	public static void main(String[] args) {
+		  // declare variables
+	    int a = 20, b = 44;
+
+	    // addition operator
+	    System.out.println("a + b = " + (a + b));
+
+	    // subtraction operator
+	    System.out.println("a - b = " + (a - b));
+
+	    // multiplication operator
+	    System.out.println("a * b = " + (a * b));
+
+	    // division operator
+	    System.out.println("a / b = " + (a / b));
+
+	    // modulo operator
+	    System.out.println("a % b = " + (a % b));
+	  }
+
+}
+
+	
+
+
